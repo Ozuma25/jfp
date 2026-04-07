eefb21fb5f738359cbee67470421e112bb8aa084
Vishal <vishalvichu31@gmail.com>
2026-04-07 19:26:11 +0530

    eslin disabled

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,11 +4,16 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
+      { protocol: "https", hostname: "jfp-backend-qv9x.onrender.com", pathname: "/media/**" },
       { protocol: "http", hostname: "127.0.0.1", port: "8000", pathname: "/media/**" },
       { protocol: "http", hostname: "localhost", port: "8000", pathname: "/media/**" },
       { protocol: "https", hostname: "images.unsplash.com", pathname: "/**" },
       { protocol: "https", hostname: "picsum.photos", pathname: "/**" },
     ],
+  },
+
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
